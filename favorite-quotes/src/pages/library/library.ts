eb90bb2fe52eb54113b28a9c905e66cc5b7ea46d
davidokun <davidokun99@gmail.com>
2018-04-05 08:45:23 -0500

    quotes - Creating the required pages.

---
+++ favorite-quotes/src/pages/library/library.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html',
+})
+export class LibraryPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+}
