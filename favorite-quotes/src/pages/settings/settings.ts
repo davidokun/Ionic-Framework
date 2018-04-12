e6e43673de65b67288c46aad0bd340345d3238ab
davidokun <davidokun99@gmail.com>
2018-04-12 09:14:47 -0500

    quotes - Preparing the Settings Page.

--- favorite-quotes/src/pages/settings/settings.ts
+++ favorite-quotes/src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, Toggle} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -11,4 +11,7 @@ export class SettingsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  onToggle(toggle: Toggle) {
+    console.log(toggle);
+  }
 }
