12354bb989148d7cfc795ed09a90ce51f2f21e00
davidokun <davidokun99@gmail.com>
2018-04-16 10:05:45 -0500

    components - Assigment solution.

---
+++ assigment-2/src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ResetComponent } from './reset/reset';
+
+@NgModule({
+	declarations: [ResetComponent],
+	imports: [],
+	exports: [ResetComponent]
+})
+export class ComponentsModule {}
