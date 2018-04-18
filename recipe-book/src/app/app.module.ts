8b4cd4eded1eba2f30783820c7f135fdb22c6dbc
davidokun <davidokun99@gmail.com>
2018-04-18 08:53:58 -0500

    recipes - Creating the required Pages.

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -5,21 +5,38 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {EditRecipePageModule} from "../pages/edit-recipe/edit-recipe.module";
+import {RecipePageModule} from "../pages/recipe/recipe.module";
+import {RecipesPageModule} from "../pages/recipes/recipes.module";
+import {ShoppingListPageModule} from "../pages/shopping-list/shopping-list.module";
+import {TabsPageModule} from "../pages/tabs/tabs.module";
+import {EditRecipePage} from "../pages/edit-recipe/edit-recipe";
+import {RecipePage} from "../pages/recipe/recipe";
+import {RecipesPage} from "../pages/recipes/recipes";
+import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
+import {TabsPage} from "../pages/tabs/tabs";
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    EditRecipePageModule,
+    RecipePageModule,
+    RecipesPageModule,
+    ShoppingListPageModule,
+    TabsPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ recipe-book/src/pages/edit-recipe/edit-recipe.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-edit-recipe',
+  templateUrl: 'edit-recipe.html',
+})
+export class EditRecipePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+}
