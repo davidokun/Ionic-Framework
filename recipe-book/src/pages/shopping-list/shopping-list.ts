a368a5c9d43fdb9556b9d3861e14d11806f447b9
davidokun <davidokun99@gmail.com>
2018-04-18 10:48:11 -0500

    recipes - Displaying items from the Shopping List.

--- recipe-book/src/pages/shopping-list/shopping-list.ts
+++ recipe-book/src/pages/shopping-list/shopping-list.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {ShoppingListService} from "../../services/shopping-list.service";
+import {IngredientModel} from "../../models/ingredient.model";
 
 @IonicPage()
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html'
 })
-export class ShoppingListPage {
+export class ShoppingListPage implements OnInit {
+
+  ingredients: IngredientModel[];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
   onAddItem(f: NgForm) {
     this.shoppingListService.addIngredient(f.value.iname, f.value.amount);
+    this.loadItems();
     f.reset();
+  }
+
+  private loadItems() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 }
