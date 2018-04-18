65f48347752902f8fd9ca5dd92af938cfac98592
davidokun <davidokun99@gmail.com>
2018-04-18 10:06:53 -0500

    recipes - Managing Data with a Service (Shopping List Service.)

---
+++ recipe-book/src/services/shopping-list.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from "@angular/core";
+import {IngredientModel} from "../models/ingredient.model";
+
+@Injectable()
+export class ShoppingListService {
+
+  private ingredients: IngredientModel[] = [];
+
+  addIngredient(name: string, amount: number) {
+    this.ingredients.push(new IngredientModel(name, amount));
+  }
+
+  addIngredients(items: IngredientModel[]) {
+    this.ingredients.push(...items);
+  }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+  }
+
+}
