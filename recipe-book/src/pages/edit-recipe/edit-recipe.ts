dbf7f6df59eaa900686c490d1430a6c1047b3fa6
davidokun <davidokun99@gmail.com>
2018-04-19 09:09:37 -0500

    recipes - Creating an Action Sheet (Reactive)

--- recipe-book/src/pages/edit-recipe/edit-recipe.ts
+++ recipe-book/src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ActionSheetController, IonicPage, NavController, NavParams} from 'ionic-angular';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @IonicPage()
@@ -13,7 +13,8 @@ export class EditRecipePage implements OnInit {
   private selectOptions = ['Easy', 'Medium', 'Hard'];
   private recipeForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit(): void {
     this.mode = this.navParams.get('mode');
@@ -24,6 +25,31 @@ export class EditRecipePage implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onManageIngredients() {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Add Ingredient',
+          handler: () => {
+
+          }
+        },
+        {
+          text: 'Remove all Ingredients',
+          role: 'destructive',
+          handler: () => {
+
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+  }
+
   private initializeForm() {
     this.recipeForm = new FormGroup({
       'title': new FormControl(null, Validators.required),
@@ -31,9 +57,4 @@ export class EditRecipePage implements OnInit {
       'difficulty': new FormControl(this.selectOptions[1], Validators.required)
     });
   }
-
-
-
-
-
 }
