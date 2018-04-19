d63a87e1cd4787ab86c1b8bf3982d32713b22b3d
davidokun <davidokun99@gmail.com>
2018-04-19 08:40:15 -0500

    recipes - Creating a Edit Recipe Form (Reactive)

--- recipe-book/src/pages/edit-recipe/edit-recipe.ts
+++ recipe-book/src/pages/edit-recipe/edit-recipe.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -6,9 +6,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
+  private mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+  ngOnInit(): void {
+    this.mode = this.navParams.get('mode');
+  }
+
+
+
+
 
 }
