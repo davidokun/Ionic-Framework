abfdac75a685f2f79de6d67e2e76861d7c381029
davidokun <davidokun99@gmail.com>
2018-04-24 08:57:59 -0500

    recipes - Creating the Signup Page (and Form)

--- recipe-book/src/pages/signup/signup.ts
+++ recipe-book/src/pages/signup/signup.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {NgForm} from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -18,8 +12,7 @@ export class SignupPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+  onSignup(form: NgForm) {
+    console.log(form.value);
   }
-
 }
