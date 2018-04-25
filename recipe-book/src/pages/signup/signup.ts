c01af8a4f7e9089a294469d24587b4a67108b34f
davidokun <davidokun99@gmail.com>
2018-04-25 16:17:16 -0500

    recipes - Showing a Loader (Spinner) and Error Alert.

--- recipe-book/src/pages/signup/signup.ts
+++ recipe-book/src/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController, IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 
 import {AuthService} from "../../services/auth.service";
@@ -12,12 +12,32 @@ import {AuthService} from "../../services/auth.service";
 export class SignupPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              private authService: AuthService) {
+              private authService: AuthService, private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) {
   }
 
   onSignup(form: NgForm) {
+
+    const loading = this.loadingCtrl.create({
+      content: 'Signing you up...'
+    });
+
+    loading.present();
+
     this.authService.signUp(form.value.email, form.value.password)
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+      .then(
+        data => {
+          loading.dismiss();
+        })
+      .catch(
+        error => {
+          loading.dismiss();
+          const alert = this.alertCtrl.create({
+            title: 'Signup Failed!',
+            message: error.message,
+            buttons: ['Ok']
+          });
+          alert.present();
+        });
   }
 }
