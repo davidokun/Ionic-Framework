1b673b137f123451f267ebfb9ceaccab0d110243
davidokun <davidokun99@gmail.com>
2018-04-25 16:01:16 -0500

    recipes - Setting up Firebase (as a Development Backend).

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, ViewChild} from '@angular/core';
 import {MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+
 import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
 import {SignupPage} from "../pages/signup/signup";
@@ -17,6 +19,12 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
               private menuCtrl: MenuController) {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyBHUpO9iE9bJFgSuBkr4UzeFuCQ8ST-fn0",
+      authDomain: "ionic-recipe-book-9998e.firebaseapp.com"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
