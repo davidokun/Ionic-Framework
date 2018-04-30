3e14788e920ac2833e1a5a33067c39cb2de23fbd
davidokun <davidokun99@gmail.com>
2018-04-30 11:52:36 -0500

    awesome-places - Passing the Chosen Location back to the Page.

--- awesome-places/src/pages/set-location/set-location.ts
+++ awesome-places/src/pages/set-location/set-location.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 import {LocationModel} from "../../models/location.model";
 
 @IonicPage()
@@ -12,11 +12,20 @@ export class SetLocationPage {
   location: LocationModel;
   marker: LocationModel;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private viewCtrl: ViewController) {
     this.location = this.navParams.get('location');
   }
 
   onSetMarker(event: any) {
     this.marker = new LocationModel(event.coords.lat, event.coords.lng);
+  }
+
+  onConfirm() {
+    this.viewCtrl.dismiss({location: this.marker});
+  }
+
+  onAbort() {
+    this.viewCtrl.dismiss();
   }
 }
